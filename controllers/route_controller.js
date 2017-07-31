e2d06f976050df79d84a051f51a2a6e8173f7ed1
Tomcariello <tomcariello@gmail.com>
2017-07-31 19:38:06 -0400

    obfuscating user keys & tokens

--- controllers/route_controller.js
+++ controllers/route_controller.js
@@ -16,12 +16,12 @@ console.log("**************************************");
 console.log(connection.config);
 console.log("**************************************");
 //Assign Dev Environment Vars if in Dev environment
-// if (process.env.JAWSDB_URL) {
-//   connection.config.host = process.env.host;
-//   connection.config.user = process.env.username;
-//   connection.config.password = process.env.password;
-//   connection.config.database = process.env.database;
-// }
+if (process.env.JAWSDB_URL) {
+  connection.config.host = process.env.host;
+  connection.config.user = process.env.username;
+  connection.config.password = process.env.password;
+  connection.config.database = process.env.database;
+}
 
 console.log("**************************************");
 
